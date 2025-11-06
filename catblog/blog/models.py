8e3cfc26f5c4c533b433f00e69289bf627a36f8b
Vantied <mrvano555@gmail.com>
2025-11-06 17:15:43 +0600

    Добавил базовые настройки, сделал две модели: Post и Category

--- catblog/blog/models.py
+++ catblog/blog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# Получем пользователя
+User = get_user_model()
+
+
+class Category(models.Model):
+    """Модель для определения категории"""
+    name = models.CharField(
+        verbose_name='Категория'
+    )
+    slug = models.SlugField(
+        max_length=128,
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text='Максимальная длина строки — 128 символов'
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=128,
+        verbose_name='Заголовок',
+        help_text='Максимальная длина строки — 128 символов'
+    )
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем'
+        ' — можно делать отложенные публикации.'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор публикации'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=False,
+        verbose_name='Категория',
+        related_name='posts'
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='blog_images',
+        blank=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
--- catblog/catblog/settings.py
+++ catblog/catblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',  # Подключил приложение blog
 ]
 
 MIDDLEWARE = [
@@ -110,11 +111,29 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Директория для хранение изображений.
+MEDIA_ROOT = BASE_DIR / 'media'
+# Перенавправление писем.
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Директория для хранение писем.
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+# Перенаправление после выхода.
+LOGOUT_REDIRECT_URL = 'blog:index'
+# Перенапрваление после входа.
+LOGIN_REDIRECT_URL = 'blog:index'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Директория для поиска статики.
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+]
+
+# Директория для поиска хранения шаболов.
+TEMPLATES_DIR = BASE_DIR / 'templates'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
